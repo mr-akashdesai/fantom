90196d328247c3d697e921d2c657ab12ac1525ea
Akash Desai <mr.akashdesai@gmail.com>
2024-07-21 15:37:34 +0100

    update config

--- forge.configure.js
+++ forge.configure.js
@@ -1,80 +1,80 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable quotes */
-const packageJson = require('./package.json')
-const { version } = packageJson
+const packageJson = require("./package.json");
+const { version } = packageJson;
 
 const commonLinuxConfig = {
   icon: {
-    scalable: './src/assets/images/logo/fantom-logo-lg.svg'
-  }
-}
+    scalable: "./src/assets/images/logo/fantom-logo-lg.svg",
+  },
+};
 
 const config = {
   packagerConfig: {
-    icon: './src/assets/images/logo/fantom-logo.icns'
+    icon: "./src/assets/images/logo/fantom-logo.icns",
   },
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      platforms: ['win32'],
-      config: arch => ({
-        name: 'fantom',
-        iconUrl: './src/assets/images/logo/fantom-logo.ico',
+      name: "@electron-forge/maker-squirrel",
+      platforms: ["win32"],
+      config: (arch) => ({
+        name: "fantom",
+        iconUrl: "./src/assets/images/logo/fantom-logo.ico",
         setupExe: `fantom-${version}-win32-${arch}-setup.exe`,
-        noMsi: true
-      })
+        noMsi: true,
+      }),
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin']
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
-      platforms: ['linux'],
-      config: commonLinuxConfig
+      name: "@electron-forge/maker-deb",
+      platforms: ["linux"],
+      config: commonLinuxConfig,
     },
     {
-      name: '@electron-forge/maker-rpm',
-      platforms: ['linux'],
-      config: commonLinuxConfig
-    }
+      name: "@electron-forge/maker-rpm",
+      platforms: ["linux"],
+      config: commonLinuxConfig,
+    },
   ],
   publishers: [
     {
-      name: '@electron-forge/publisher-github',
+      name: "@electron-forge/publisher-github",
       config: {
         repository: {
-          owner: 'mr-akashdesai',
-          name: 'fantom'
+          owner: "mr-akashdesai",
+          name: "fantom",
         },
         prerelease: false,
-        draft: true
-      }
-    }
+        draft: true,
+      },
+    },
   ],
   plugins: [
-    [
-      '@electron-forge/plugin-webpack',
-      {
-        mainConfig: './webpack.main.config.js',
+    {
+      name: "@electron-forge/plugin-webpack",
+      config: {
+        mainConfig: "./webpack.main.config.js",
         devContentSecurityPolicy:
           "default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;",
         renderer: {
-          config: './webpack.renderer.config.js',
+          config: "./webpack.renderer.config.js",
           entryPoints: [
             {
-              html: './src/index.html',
-              js: './src/electron/renderer.tsx',
-              name: 'main_window',
+              html: "./src/index.html",
+              js: "./src/electron/renderer.tsx",
+              name: "main_window",
               preload: {
-                js: './src/electron/preload.ts'
-              }
-            }
-          ]
-        }
-      }
-    ]
-  ]
-}
+                js: "./src/electron/preload.ts",
+              },
+            },
+          ],
+        },
+      },
+    },
+  ],
+};
 
-module.exports = config
+module.exports = config;
